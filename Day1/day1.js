a5f593c0d9a84b44af8ad5f5675a8c12b3de1135
Thomas Anda <thomas.anda@appliedtechnology.se>
2021-12-02 20:33:30 +0100

    did not see part two of day 1

--- Day1/day1.js
+++ Day1/day1.js
@@ -1,5 +1,7 @@
-
 const fs = require('fs');
+
+//Part 1
+
 fs.readFile('./inputDay1.txt', 'utf8', (err, data) => {
   if (err) {
     console.error(err);
@@ -14,5 +16,21 @@ fs.readFile('./inputDay1.txt', 'utf8', (err, data) => {
 })
 
 
+// Part 2
+
+fs.readFile('./inputDay1.txt', 'utf8', (err, data) => {
+  if (err) {
+    console.error(err);
+    return;
+  }
+  const numbers = data.split('\n').map(number => Number(number));
+  let counter = 0;
+  for (let i = 0; i < numbers.length; i++) {
+    const first = numbers[i] + numbers[i + 1] + numbers[i + 2];
+    const second = numbers[i + 1] + numbers[i + 2] + numbers[i + 3];
+    first < second ? counter += 1 : null;
+  }
+  console.log(counter);
+})
 
 
